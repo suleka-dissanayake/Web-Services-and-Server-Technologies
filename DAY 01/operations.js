8227924aae905cdd921b7d05829e21373958f322
Suleka Dissanayake <sulekadissanayake2003@gmail.com>
2026-02-18 14:48:19 +0530

    Add files via upload

---
+++ DAY 01/operations.js
@@ -0,0 +1,12 @@
+a = 8;
+b = 4;
+c = a + b;
+d = a - b
+e = a * b;
+f = a / b;
+g = a % b;
+console.log("Sum is " + c);
+console.log("Substraction is " + d);
+console.log("Multiplication is " + e);
+console.log("Division is " + f);
+console.log("Modulus is " + g);
