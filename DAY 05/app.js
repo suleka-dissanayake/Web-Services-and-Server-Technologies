d707caff687826f55212fad7ae5e3fa18260a972
Suleka Dissanayake <sulekadissanayake2003@gmail.com>
2026-03-03 20:50:06 +0530

    Files

---
+++ DAY 05/app.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express()
+app.get('/', function (req, res) {
+    res.send('Hello world!');
+});
+app.listen(5000, function(){
+    console.log("app.js is listening to http://localhost:5000/")
+});
