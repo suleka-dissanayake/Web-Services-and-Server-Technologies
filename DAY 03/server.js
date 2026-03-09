4268514617737e4c5e93783e3f2a904978b21f7f
Suleka Dissanayake <sulekadissanayake2003@gmail.com>
2026-03-09 12:04:38 +0900

    Update server.js

--- DAY 03/server.js
+++ DAY 03/server.js
@@ -3,17 +3,23 @@ const fs = require('fs');
 const url = require('url');
 
 http.createServer(function(request, response) {
+
     const pathname = url.parse(request.url).pathname;
     console.log(`Request for ${pathname} received!`);
+
     fs.readFile(pathname.substring(1), function(err, data) {
+
         if (err) {
             console.log(err.stack);
-            response.writeHead(404, {'content-type':'test/html'});
-            response.end('404 not found!');
-        } else{
-            response.writeHead(200, {'content-type':'test/html'});
-            response.end();
+            response.writeHead(404, { 'Content-Type': 'text/html' });
+            response.end('<h1>404 Not Found</h1>');
+        } else {
+            response.writeHead(200, { 'Content-Type': 'text/html' });
+            response.end(data);
         }
+
     });
+
 }).listen(8081);
-console.log('Server running at http://127.0.0.1:8081...');
+
+console.log('Server running at http://127.0.0.1:8081/');
