0d77858e9389314c9c70278bb0a6aaf0c4b396d7
Suleka Dissanayake <sulekadissanayake2003@gmail.com>
2026-04-28 16:05:22 +0530

    updated

--- CRUD/index.js
+++ CRUD/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import bodyParser from 'body-parser';
 import dotenv from 'dotenv';
+import route from "./routes/userRoute.js";
 
 const app = express();
 app.use(bodyParser.json());
@@ -18,3 +19,5 @@ mongoose
             console.log(`Server is running on ${PORT}`);
         });
     }).catch((error) => console.log(error));
+
+app.use('/api/user', route);
